83b826cc5e5cef00b429922e9bc25233f9f34a0c
Zlatin Balevsky <zab@Zlatin-Balevskys-MacBook-Pro.local>
2014-08-30 23:00:43 +0100

    wip

--- src/zab/atomics/buffer/AtomicBuffer.java
+++ src/zab/atomics/buffer/AtomicBuffer.java
@@ -2,23 +2,30 @@ package zab.atomics.buffer;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * A buffer that is safe to use by multiple threads.  It has maximum capacity of 2MB.
+ * @author zlatinb
+ */
 public class AtomicBuffer {
 
 	private static final int MAX_SIZE = 21;
 
-	private final int claimMask, writtenMask, readMask;
+	private final long claimMask, writtenMask, readMask;
 
 	private final AtomicLong state = new AtomicLong();
 
 	private final byte[] data;
 
+	/**
+	 * @param sizePow2 size of the buffer as power of 2
+	 */
 	public AtomicBuffer(int sizePow2) {
 		if (sizePow2 > MAX_SIZE)
 			throw new IllegalArgumentException();
 		
 		data = new byte[1 << sizePow2];
 
-		readMask = 1 << sizePow2 - 1;
+		readMask = (1 << sizePow2) - 1;
 		claimMask = readMask << MAX_SIZE;
 		writtenMask = claimMask << MAX_SIZE;
 	}
@@ -35,6 +42,11 @@ public class AtomicBuffer {
 		return (int)((state & writtenMask) >> (MAX_SIZE << 1));
 	}
 
+	/**
+	 * put bytes into this buffer.  If the buffer is full, return false
+	 * @param src source byte[] to copy data from
+	 * @return true if all bytes were successfully put in the buffer
+	 */
 	public boolean put(byte[] src) {
 		// TODO: implement
 		return false;
@@ -43,5 +55,10 @@ public class AtomicBuffer {
 	public int get(byte[] dest) {
 		// TODO: implement
 		return 0;
+	}
+	
+	public static void main(String []ar) {
+	    AtomicBuffer ab = new AtomicBuffer(20);
+	    System.out.printf("%x %x %x\n", ab.readMask, ab.claimMask, ab.writtenMask);
 	}
 }
