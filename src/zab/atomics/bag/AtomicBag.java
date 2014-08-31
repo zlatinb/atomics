9f59644b51374fc5da748fd5571c364b7843c90b
Zlatin Balevsky <zab@Zlatin-Balevskys-MacBook-Pro.local>
2014-08-31 12:34:05 +0100

    statics, get() method

--- src/zab/atomics/bag/AtomicBag.java
+++ src/zab/atomics/bag/AtomicBag.java
@@ -22,36 +22,31 @@ public class AtomicBag<T> {
     
     private final AtomicLong state = new AtomicLong();
     
-    private long freeMask(int i) {
+    private static long freeMask(int i) {
         long mask = 1 << (i << 1);
         mask |= (mask << 1);
         return ~mask;
     }
     
-    private long free(long state, int i) {
+    private static long free(long state, int i) {
         return state & freeMask(i);
     }
     
-    private long claim(long state, int i) {
+    private static long claim(long state, int i) {
         long freed = free(state,i);
         return freed | (CLAIM << (i <<1));
     }
     
-    private long full(long state, int i) {
+    private static long full(long state, int i) {
         long freed = free(state,i);
         return freed | (FULL << (i <<1));
     }
     
-    private int find(final long state, final int type) {
-        for (int i = 0; i < 32; i++) {
-            long s = state & (~freeMask(i));
-            s >>>= (i << 1);
-            if (s == type)
-                return i;
-        }
-        return -1;
+    private static int get(long state, final int i) {
+        state &= (~freeMask(i));
+        return (int)(state >>> (i << 1));
     }
-
+    
     /**
      * @param item to store
      * @return true if stored, false if there was no space.
@@ -64,7 +59,7 @@ public class AtomicBag<T> {
     
     public static void main(String[] ar) {
         AtomicBag<?> as = new AtomicBag<Object>();
-        System.out.printf("%x\n",as.find(0,CLAIM));
+        System.out.printf("%x\n",as.get(12,1));
     }
 
 }
